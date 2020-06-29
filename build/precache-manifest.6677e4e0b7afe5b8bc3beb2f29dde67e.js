97fcf7a4aab2802fc68709f5801d10d72fe50a46
ragav <ragavavishal@gmail.com>
2020-06-29 13:50:44 +0530

    checkbox alert

---
+++ build/precache-manifest.6677e4e0b7afe5b8bc3beb2f29dde67e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43758914d683fd8df06e0c0cdd45fe15",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1496ef12c80fceab6cca",
+    "url": "/react-deploy/static/css/main.fc06d049.chunk.css"
+  },
+  {
+    "revision": "1e82320dcf34b85a04d5",
+    "url": "/react-deploy/static/js/2.fac82981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.fac82981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1496ef12c80fceab6cca",
+    "url": "/react-deploy/static/js/main.61ee4f20.chunk.js"
+  },
+  {
+    "revision": "5f7657eb60301b295f57",
+    "url": "/react-deploy/static/js/runtime-main.6b28c481.js"
+  }
+]);
